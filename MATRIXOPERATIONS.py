4b16ae9d2ee4dac2b77f8e2906c689a3a50beed3
srilekhadronadula <srireddy7789@gmail.com>
2024-08-24 14:47:09 +0530

    my 1 program

---
+++ MATRIXOPERATIONS.py
@@ -0,0 +1,13 @@
+import numpy as np
+a=[[1,2,3],[4,5,6],[7,8,9]]
+b=[[9,8,7],[6,5,4],[3,2,1]]
+add=np.add(a,b)
+sub=np.subtract(a,b)
+muliply=np.multiply(a,b)
+trace=np.trace(a)
+traceb=np.trace(b)
+print(add)
+print(sub)
+print(mul)
+print(trace)
+print(traceb)
