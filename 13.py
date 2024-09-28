558aaabb06aa8cafbcf430d65e7a79d4bd21dc2c
srilekhadronadula <srireddy7789@gmail.com>
2024-09-28 14:53:06 +0530

    new message

---
+++ 13.py
@@ -0,0 +1,16 @@
+def closest_to_sum(arr):
+    arr.sort()
+    left=0
+    right=len(arr)-1
+    closest_sum=float('inf')
+    while left<right:
+        current_sum=arr[left]+arr[right]
+        if abs(current_sum)<abs(closest_sum):
+            closest_sum=current_sum
+        if current_sum<0:
+            left+=1
+        else:
+            right-=1
+    return closest_sum
+arr=[1,5,-2,3,-1,7,-4]
+print(closest_to_zero)
